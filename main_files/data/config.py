d172e343f2d2da6c8ac72868a08cefb3c478d35a
Ekaterina-Ivanova345 <ivanova.ek92@gmail.com>
2022-11-20 21:05:51 +0300

    many minor changes applied

---
+++ main_files/data/config.py
@@ -0,0 +1,4 @@
+# This file contains database details
+
+dna_rna_relations_address = "sqlite:///dna_rna_relations.db"
+triplets_aminoacids_relations_address = "sqlite:///triplets_aminoacids_relations.db"
